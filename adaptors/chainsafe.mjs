2fbfb40190d3a5e13f87d70c220ee68492d45573
Anudit Nagar <nagaranudit@gmail.com>
2022-04-03 09:27:30 +0530

    Improve Error Handling

--- adaptors/chainsafe.mjs
+++ adaptors/chainsafe.mjs
@@ -8,36 +8,42 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 
 export async function storeOnChainsafeStorage( fn, path ){
-    console.log("🔃 Backing up data to Chainsafe Storage");
 
-    const file = fs.createReadStream(path);
-    const title = fn;
+    try {
+        console.log("🔃 Backing up data to Chainsafe Storage");
 
-    const form = new FormData();
-    form.append('title', title);
-    form.append('file', file);
+        const file = fs.createReadStream(path);
+        const title = fn;
 
-    var requestOptions = {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${CHAINSAFE_STORAGE_API_KEY}`,
-            ...form.getHeaders(),
-        },
-        body: form,
-        redirect: 'follow'
-    };
+        const form = new FormData();
+        form.append('title', title);
+        form.append('file', file);
 
-    let resp = await fetch("https://api.chainsafe.io/api/v1/bucket/ad2c9217-bf2a-4e13-a8b6-85bd375aada2/upload", requestOptions);
-    let json = await resp.json();
+        var requestOptions = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${CHAINSAFE_STORAGE_API_KEY}`,
+                ...form.getHeaders(),
+            },
+            body: form,
+            redirect: 'follow'
+        };
 
-    if ('path' in json){
-        console.log('🟢 Backup to Chainsafe Sucessful');
+        let resp = await fetch("https://api.chainsafe.io/api/v1/bucket/ad2c9217-bf2a-4e13-a8b6-85bd375aada2/upload", requestOptions);
+        let json = await resp.json();
+
+        if ('path' in json){
+            console.log('🟢 Backup to Chainsafe Sucessful');
+        }
+        else {
+            console.log('🔴 Backup to Chainsafe Failed')
+            console.log(json);
+        }
+
+        return json;
+
+    } catch (error) {
+        console.log('🔴 Chainsafe Backup Error', error);
     }
-    else {
-        console.log('🔴 Backup to Chainsafe Failed')
-        console.log(json);
-    }
-
-    return json;
 
 }
